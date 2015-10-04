269e6e35737a8bab9d098edfc423fac22b9e08b1
Buggyy <stefan.lobato.yildiz@hva.nl>
2015-10-04 14:41:22 +0200

    uitleg handler

---
+++ src/ds/pkg2/Handler.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds.pkg2;
+
+
+/**
+ *
+ * @author Stefan Lobato [IS204] 500707274
+ */
+public class Handler {
+
+    public void generateClass() {
+        // Generate class from regexr.
+        
+        //  
+    }
+
+    public void addStudent(Student student) {
+//        if (list.size() < MAX_STUDENTS) {
+//            list.add(student);
+//
+//        } else {
+//            //  generate new klas (use generate class method)
+//
+//            throw new IndexOutOfBoundsException();
+//        }
+
+    }
+    
+      public void updateClass() {
+        // 
+    }
+    
+      
+      /**
+       * Calculation i.e:
+       * 10.000 students / 5 courses = 2000 students per course
+       * 2000 / 32 = 62.5 round() = 63 classes
+       * 
+       * 
+       * 
+       */
+
+}
