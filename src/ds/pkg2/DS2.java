7a21ed3f9d6edf57cba7c9e6fff99af8ae60890d
Buggyy <stefan.lobato.yildiz@hva.nl>
2015-10-04 13:35:21 +0200

    begin van project

---
+++ src/ds/pkg2/DS2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds.pkg2;
+
+/**
+ *
+ * @author Omer Z 500.. & Stefan Lobato [IS204] 500707274
+ */
+public class DS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
