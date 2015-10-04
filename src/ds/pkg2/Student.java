fe7a8599ed5fe5f5b9cdb8a3209980aac29ab0e7
Buggyy <stefan.lobato.yildiz@hva.nl>
2015-10-04 13:57:27 +0200

    uitgebreide student class

--- src/ds/pkg2/Student.java
+++ src/ds/pkg2/Student.java
@@ -10,12 +10,40 @@ package ds.pkg2;
  * @author Omer
  */
 public class Student {
-    
+
     // Attributes
-    private int studentnummer;
-    private String klas;
+    private int studentNummer;
+    private Klas klas;
     private double cijfer;
-    
-    
-    
+
+    public Student(int studentNummer, Klas klas, double cijfer) {
+        this.studentNummer = studentNummer;
+        this.klas = klas;
+        this.cijfer = cijfer;
+    }
+
+    public int getStudentNummer() {
+        return studentNummer;
+    }
+
+    public void setStudentNummer(int studentNummer) {
+        this.studentNummer = studentNummer;
+    }
+
+    public Klas getKlas() {
+        return klas;
+    }
+
+    public void setKlas(Klas klas) {
+        this.klas = klas;
+    }
+
+    public double getCijfer() {
+        return cijfer;
+    }
+
+    public void setCijfer(double cijfer) {
+        this.cijfer = cijfer;
+    }
+
 }
