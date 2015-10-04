cc681eb52cd7a4ce7334a3daa2d68f610a27755a
Omer Zulaloglu <Omer@MBP.local>
2015-10-04 14:07:03 +0200

    klasse: Klas toegevoegd

---
+++ src/ds/pkg2/Klas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds.pkg2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Omer
+ */
+public class Klas {
+    
+    // Attributes
+    private ArrayList<Student> list = new ArrayList<>();
+    private String naam;
+    
+    // Constructor
+    public Klas(){
+        
+    }
+    
+    public void addStudent(Student student){
+        list.add(student);
+    }
+    
+    public ArrayList getList(){
+        return this.list;
+    }
+    
+    public String getNaam(){
+        return this.naam;
+    }
+    
+}
